b1d0ecd793461282e7e670a4bacaa3329ed06d14
Shane Massaad <shanemassaad@hotmail.com>
2021-03-07 20:40:13 +0000

    Replace updateText with separate functions for each property

--- Headers/TextManager.h
+++ Headers/TextManager.h
@@ -13,7 +13,11 @@ private:
 
 public:
 	void createText(const std::string& name, const std::string& fontName, int size, sf::Color color, std::string string, float xpos, float ypos);
-	void updateText(const std::string& textName, const std::string& fontName, int size, sf::Color color, std::string string, float xpos, float ypos);
+	void updateTextString(const std::string& textName, std::string string);
+	void updateTextColor(const std::string& textName, sf::Color color);
+	void updateTextSize(const std::string& textName, int size);
+	void updateTextPosition(const std::string& textName, float xpos, float ypos);
+	void updateTextFont(const std::string& textName, const std::string& fontName);
 
 	// Translate an id into a reference
 	sf::Text& getTextRef(const std::string& text);
--- src/TextManager.cpp
+++ src/TextManager.cpp
@@ -18,16 +18,32 @@ void TextManager::createText(const std::string& name, const std::string& fontNam
 	texts[name] = text;
 }
 
-void TextManager::updateText(const std::string& textName, const std::string& fontName, int size, sf::Color color, std::string string, float xpos, float ypos) {
+void TextManager::updateTextString(const std::string& textName, std::string string) {
 	sf::Text text = getTextRef(textName);
-	sf::Font font = fontManager->getFontRef(fontName);
-	
-	text.setFont(font);
-	text.setCharacterSize(size);
-	text.setFillColor(color);
 	text.setString(string);
+}
+
+void TextManager::updateTextColor(const std::string& textName, sf::Color color) {
+	sf::Text text = getTextRef(textName);
+	text.setFillColor(color);
+}
+
+void TextManager::updateTextSize(const std::string& textName, int size) {
+	sf::Text text = getTextRef(textName);
+	text.setCharacterSize(size);
+}
+
+void TextManager::updateTextPosition(const std::string& textName, float xpos, float ypos) {
+	sf::Text text = getTextRef(textName);
 	text.setPosition(xpos, ypos);
 }
+
+void TextManager::updateTextFont(const std::string& textName, const std::string& fontName) {
+	sf::Text text = getTextRef(textName);
+	sf::Font font = fontManager->getFontRef(fontName);
+	text.setFont(font);
+}
+
 
 sf::Text& TextManager::getTextRef(const std::string& text) {
 	return texts.at(text);
