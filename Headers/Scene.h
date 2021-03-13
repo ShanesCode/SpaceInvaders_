ed0af560d6ff7351461159411c4f451006567525
Shane Massaad <shanemassaad@hotmail.com>
2021-03-13 11:06:27 +0000

    Remove deprecated code from Scene class

--- Headers/Scene.h
+++ Headers/Scene.h
@@ -3,20 +3,6 @@
 
 class Scene {
 	public:
-		//std::string sceneName;
-
-		//sf::Text createText(const sf::Font& font, int size, sf::Color color, std::string string, float xpos, float ypos);
-		//void updateText(int textVectorIndex, const sf::Font& font, int size, sf::Color color, std::string string, float xpos, float ypos);
-
-		//std::vector<sf::Text> textVector;
-
-		//virtual void createTextElements(Config& config);
-
-		//sf::Music music;
-		//std::string musicString;
-
-		//virtual void playMusic(bool loop, float volume);
-
 		Game* game;
 
 		virtual void draw(const float dt) = 0;
@@ -24,6 +10,4 @@ class Scene {
 		virtual void update(const float dt) = 0;
 
 		virtual void handleInput() = 0;
-		
-		//Scene(std::string sceneName_, std::string musicString_, Config& config);
 };
