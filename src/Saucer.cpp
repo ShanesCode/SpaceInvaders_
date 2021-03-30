f2eff8956a9c2156a75f798de618c37f69a789e6
Shane Massaad <shanemassaad@hotmail.com>
2021-03-30 19:48:05 +0100

    Create stubs for the Saucer entity

---
+++ src/Saucer.cpp
@@ -0,0 +1,25 @@
+#include "../Headers/Saucer.h"
+
+Saucer::Saucer(int health_, int movespeed_, int fireRate_, float x_location_, float y_location_)
+	: Entity(health_, movespeed_, fireRate_, x_location_, y_location_) {
+	health = health_;
+	movespeed = movespeed_;
+	fireRate = fireRate_;
+	x_location = x_location_;
+	y_location = y_location_;
+	alive = true;
+}
+
+void Saucer::move(const float dt) {
+	// Move horizontally of block
+
+}
+
+void Saucer::death() {
+	if (health == 0) {
+		alive = false;
+		// play death animation
+
+		// remove it from the list of enemies? something like that
+	}
+}
